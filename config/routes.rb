2dff94db338a0e220ab4abbd376ea55bb3633bf1
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-08-26 10:59:40 +0500

    Setting up Home page
    - adding new Home Controller
    - adding index and about actions and pages
    -setting up new home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index' => 'home#index', as: 'welcome_page'
+  get 'home/about' => 'home#about', as: 'about_page'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
