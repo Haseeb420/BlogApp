80906ddc1c86d04d36782421e1d49dab8ab172d8
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-06 14:05:54 +0500

    adding smtp for production
    adding smtp for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,5 +98,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:             Rails.application.credentials.dig(:gmail_creds, :gmail_username),
+    password:             Rails.application.credentials.dig(:gmail_creds, :gmail_password),
+    authentication:       'plain'
+  }
 end
 
