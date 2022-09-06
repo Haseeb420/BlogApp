9007efaea64413ab17176dd82b582f1e548803c3
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-06 13:48:35 +0500

    replacing uglifier with terser
    - gem 'terser', '~> 1.1', '>= 1.1.12'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,10 @@ Rails.application.configure do
   end
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
-  config.assets.css_compressor = :sass
+  # config.assets.js_compressor = Uglifier.new(:harmony => true)
+  # config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :yui
+  config.assets.js_compressor = :terser
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
