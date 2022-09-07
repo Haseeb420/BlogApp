9c285a46a17ac6d52157d248a345571ed65fa50c
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-07 11:58:54 +0500

    adding user image ni nav
    adding user image ni nav

--- db/migrate/20220826071926_add_details_to_user.rb
+++ db/migrate/20220826071926_add_details_to_user.rb
@@ -2,9 +2,5 @@ class AddDetailsToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :username, :string
-    add_column :users, :about, :text
-    add_column :users, :birthday, :date
-    add_column :users, :number_of_comments, :integer, default:0
   end
 end
