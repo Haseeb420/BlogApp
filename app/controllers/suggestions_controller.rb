b9dc67a3b0a8ef9168bccc1f8c2fc3f6e7d4c773
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-19 09:20:17 +0500

    making fixes after merge conflict
    making fixes after merge conflict

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,7 +5,7 @@ class SuggestionsController < ApplicationController
   before_action :build_suggestion, only: [:create]
 
   def index
-    @suggestions = Suggestion.all
+    @suggestions = Suggestion.all_suggestions
     render layout: "moderator_dashboard"
   end
 
@@ -20,7 +20,13 @@ class SuggestionsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @suggestion = @post.suggestions.find(params[:id])
+    @suggestion.delete
+    respond_to do |format|
+      format.html { redirect_to suggestions_path }
+    end
+  end
 
   private
     def suggestion_params
@@ -29,5 +35,8 @@ class SuggestionsController < ApplicationController
 
     def build_suggestion
       @suggestion = @post.suggestions.build(suggestion_params)
+    end
+    def set_suggestion
+      @suggestion = Suggestion.find(params[:id])
     end
 end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,4 +2,7 @@
 
 class Suggestion < ApplicationRecord
   belongs_to :post
+
+  scope :all_suggestions, -> { Suggestion.all.order(created_at: :desc)}
+  # Ex:- scope :active, -> {where(:active => true)}
 end
