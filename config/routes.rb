41d656f1f3b65783369e0216e406edeef9f91d58
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-19 11:52:02 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resource :post_like, only: :show
     resources :comments do
       collection do
-        get :comment_reply
+        post :comment_reply
       end
       resources :comment_likes
     end
@@ -19,8 +19,9 @@ Rails.application.routes.draw do
   resources :images, only: %i[show create]
   get "home/index" => "home#index", as: "welcome_page"
   get "home/about" => "home#about", as: "about_page"
-
   post "/posts/:post_id/comments-reply" => "comments#reply", as: "comment_reply"
+
+
   # moderators routes starts here
   scope "/moderator" do
     resources :posts do
@@ -37,12 +38,6 @@ Rails.application.routes.draw do
   end
 
   get "moderators/dashboard" => "moderators#index", as: "moderator_dashboard"
-  # get "moderators/recent-post" => "moderators#recent_post", as: "moderator_recent_post"
-  # get "moderators/post-approval/(:post_id)" => "moderators#approved_post", as: "post_approval"
-  # get "moderators/reported-posts" => "moderators#reported_post", as: "reported_post"
-  # get "moderators/post-details/(:post_id)" => "moderators#post_details", as: "post_details"
-  # get "moderators/suugestions/" => "moderators#suggestions", as: "all_suggestions"
-  # get "moderators/delete-suggestions/:id" => "moderators#delete_suggestions", as: "delete_suggestions"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
