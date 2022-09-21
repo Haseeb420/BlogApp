2687d444044006379ac669e57acc51c213adafb0
Muhammad Haseeb Malik <109809119+MalikHaseeb420@users.noreply.github.com>
2022-09-21 12:32:09 +0500

    Update post_category_spec.rb

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -3,6 +3,13 @@
 require "rails_helper"
 
 RSpec.describe PostCategory, type: :model do
+  describe "validations" do
+    describe "category name" do
+      it "must be present" do
+        @category = PostCategory.new(category_name: "category_name")
+      end
+    end
+  end
   subject {
      described_class.new(category_name: "category1")
    }
